ebeeb12f44ab6f7a7fc785b52267b1b77a175ccd
PolJaimejuanCaubet <poljaimejuan@HUAWEI-POLETE>
2024-03-08 11:48:26 +0100

    branchPolete

---
+++ test1/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Student
+
+# Create your views here.
+def hello_world(request):
+    return HttpResponse("Hello world!")
+
+def get_students(request):
+    students = Student.objects.all()
+    student_values = list(students.values())
+    return JsonResponse(student_values, safe=False)
+
+def get_website(request):
+    return render(request, 'test.html')
+
+def saludo(request):
+    return HttpResponse("Hola como estas?")
