0293abca3a25f0635e081ea3b211f8e432a55bf4
Adrià Creus Nuñez <adriacreus03@gmail.com>
2024-05-16 22:40:06 +0200

    Url problems fixed and templates created (project reorganized)

--- test1/app/urls.py
+++ test1/app/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from . import views
 from .views import *
 
+
 urlpatterns = [
     # path('students/', get_students),
-    path('', home_noacc),
-    path('home/', home),
-    path('seleccionar-dia/',seleccionar_dia, name='seleccionar_dia'),
-
+    path('', home, name="home"),
+    path('home/', home, name="home"),
+    path('aboutUs/', aboutUs, name='aboutUs'),
+    path('contacto/', contacto, name='contacto'),
+    path('seleccionar-dia/', seleccionar_dia, name='seleccionar_dia'),
 
 ]
